bc488f30050ac16f7c3d92acbf607f03c747d1e4
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-08-17 15:04:13 +0300

    fix: adding authentification on request

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
 import express, { type Express, type Request, type Response } from 'express';
-import type { Student } from '../src/model/Student.ts';
-import { getAllStudents, createStudent } from './controller/studentController.ts';
+import helmet from 'helmet';
+import cors from 'cors';
+import { getAllStudents, createStudent ,deleteStudent,updateStudent} from './controller/studentController.ts';
+import { errorHandler } from './security/errorHandler.middleware.ts';
+import { authenticateToken } from './security/auth.middleware.ts';
 const app: Express = express();
-app.get('/students' , getAllStudents );
-app.post('/students',createStudent);
+app.use(helmet());
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN || '*' })); // à restreindre en prod
+app.use(express.json({ limit: '10kb' })); 
+app.get('/students' ,authenticateToken, getAllStudents );
+app.post('/students',authenticateToken, createStudent);
+app.delete('/students/:id', authenticateToken, deleteStudent);
+app.put('/students/:id', authenticateToken, updateStudent);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.post('/create', (req: Request, res: Response) => {
-  res.send('Create request received');
-});
-
-app.delete('/delete/{id}',(req: Request, res: Response) => {
-  const {id} = req.params;
-  res.send(`Delete request received for ID : ${id}`)
-});
-
+app.use(errorHandler);
 app.listen(3000);
